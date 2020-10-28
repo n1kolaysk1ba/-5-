2318b1a86f9bb509c05fbc680048d51c92b4dd23
Nikolay <73244584+n1kolaysk1ba@users.noreply.github.com>
2020-10-28 15:06:51 +0200

    add "ОП 5 Скиба.cpp"

---
+++ ОП 5 Скиба.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	double a, b;
+
+	cout << "a*b" << endl;
+	cout << "input a: " << endl;
+	cin >> a;
+
+	cout << "input b: " << endl;
+	cin >> b;
+
+	cout << a * b << endl;
+
+	system("pause");
+	return 0;
+
+}
