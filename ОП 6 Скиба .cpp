b621448086aa5af932e245b48ff53848ee79c347
n1kolaysk1ba <skiba.andrew2011yandex.ua@gmail.com>
2020-11-30 14:57:39 +0200

    add file:ОП 6 Скиба

---
+++ ОП 6 Скиба .cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include "windows.h"
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	float x, y, x1, x2, h;
+
+	cout << "Введіть нижню межу (х1)" << endl;
+	cin >> x1;
+
+	cout << "Введіть верхню межу (х2)" << endl;
+	cin >> x2;
+
+	cout << "Введіть крок табулювання (h)" << endl;
+	cin >> h;
+
+	x = x1;
+
+	cout << "Результат:" << endl;
+
+	while (x <= (x2 + h / 2))
+	{
+		y = pow(x, 3) + 2 * log(abs(x)) + 3;
+		cout << setw(10) << "x=" << x << setw(10) << "y=" << y << endl;
+		x += h;
+
+	}
+
+	system("pause");
+	return 0;
+}
